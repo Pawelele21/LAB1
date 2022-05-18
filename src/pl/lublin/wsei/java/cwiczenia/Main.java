0a2a3880ee13ad1cf9001d3ee91cfde175a6b933
Pawelele21 <zalewskipawel603@gmail.com>
2022-05-18 12:56:50 +0200

    KROK 1,2,3 a) b)

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -2,8 +2,13 @@ package pl.lublin.wsei.java.cwiczenia;
 
 /**komentarz 3*/
 
-public class Main {
 
+public class Main {
+    /**komentarz 3*/
+    /**
+     * javadoc
+     * @param args argumenty pobrane z wiersza poleceń
+     */
     public static void main(String [] args) {
         //komentarz 1
         /*komentarz2*/
