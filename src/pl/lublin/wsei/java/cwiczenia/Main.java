aed6a95dd9fa00116c8ed2731ef8a21a26434134
Pawelele21 <zalewskipawel603@gmail.com>
2022-05-18 13:06:25 +0200

    Podłączanie zewn. biblioteki do projektu, tworzenie klasy...

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -5,7 +5,10 @@ package pl.lublin.wsei.java.cwiczenia;
 
 public class Main {
     /**komentarz 3*/
-
+    /**
+     * javadoc
+     * @param args argumenty pobrane z wiersza poleceń
+     */
     public static void main(String [] args) {
         //komentarz 1
         /*komentarz2*/
