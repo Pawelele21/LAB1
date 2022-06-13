fb8b15467e0fd32d5900e9ad496d2243385c3e53
Pawelele21 <zalewskipawel603@gmail.com>
2022-06-13 15:01:18 +0200

    Podłączanie zewn. biblioteki do projektu, tworzenie klasy...

--- src/pl/lublin/wsei/java/cwiczenia/Main.java
+++ src/pl/lublin/wsei/java/cwiczenia/Main.java
@@ -19,6 +19,62 @@ public class Main {
         */
         System.out.println("Hello Java world\n");
 
+        System.out.println("Metoda print bez znaczników końca linii");
+
+        System.out.print("Ala");
+        System.out.print("ma");
+        System.out.print("kota");
+
+        System.out.println("Metoda println");
+
+        System.out.println("Ala");
+        System.out.println("ma");
+        System.out.println("kota");
+
+        System.out.println("Metoda print z użyciem znaczników końca linii");
+
+        System.out.print("Aln\n");
+        System.out.print("ma\n");
+        System.out.print("kota\n");
+
+        //-----------------------------------------------------------------------------------------------------------
+
+        System.out.print("\nDeklaracja zmiennych typów int, double, string\n");
+
+        int a = 3;
+        double b = 4.21;
+        String s = "jakiś tekst";
+
+        System.out.printf("a = %d, b = %.2f, s= %20s %n", a,b,s);
+
+        //-----------------------------------------------------------------------------------------------------------
+
+        System.out.println("\nNazwaliśmy to \"witaminą B3\"");
+
+        //-----------------------------------------------------------------------------------------------------------
+
+        System.out.print("\nTabelka funkcji SIN");
+
+        System.out.print("\nalfa\tsin(alfa)\n");
+        for (int i = 0; i < 370; i+=10) {
+            System.out.printf("%d\t\t%f\t\n", i, Math.sin(i/360.0*2*Math.PI));
+        }
+        //-----------------------------------------------------------------------------------------------------------
+
+        System.out.print("Tabelka loagarytmów o podstawie 2 i akumulowana suma kolejnych potęg liczby 2");
+        System.out.print("\narg\t\tlog2(arg)\tsum(arg)\n");
+
+        int arg = 1, sum = 0;
+        double log;
+
+        do {
+            log = Math.log(arg) /Math.log(2);
+            sum += arg;
+            System.out.printf("%d\t\t\t%.0f\t\t\t%d\n", arg, log, sum);
+            arg *= 2;
+        } while (arg < 20);
+
+        //-----------------------------------------------------------------------------------------------------------
 
     }
 
